336fe17e9c353559aaac0cb3e9d4d134ffbc34ae
BuiHuyHieu <hieudevsoft@gmail.com>
2020-12-03 00:14:04 +0700

    done app

--- app/src/main/java/com/example/mynote/Adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/mynote/Adapter/NoteAdapter.kt
@@ -1,23 +1,28 @@
 package com.example.mynote.Adapter
 
-import android.app.Activity
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mynote.Fragment.main_screenDirections
 import com.example.mynote.R
 import com.example.mynote.Room.Note
 import kotlinx.android.synthetic.main.row_layout.view.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 
-class NoteAdapter(context: Context):
-    RecyclerView.Adapter<NoteAdapter.MyViewHolder>() {
-    private var list = emptyList<Note>()
-    var context:Context = context
+class NoteAdapter(context: Context, listNote: List<Note>) :
+    RecyclerView.Adapter<NoteAdapter.MyViewHolder>(), Filterable {
+    private var list = listNote
+    private var fullList: ArrayList<Note> = ArrayList(listNote)
+    var context: Context = context
     private var lastPosition = -1
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.MyViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.row_layout, null)
@@ -27,7 +32,7 @@ class NoteAdapter(context: Context):
     override fun onBindViewHolder(holder: NoteAdapter.MyViewHolder, position: Int) {
         val item = list[position]
         holder.setData(item)
-        setAnimation(holder.itemView,position)
+        setAnimation(holder.itemView, position, false)
         holder.itemView.setOnClickListener {
             val action = main_screenDirections.actionMainScreenToEditNote(item)
             Navigation.findNavController(holder.itemView).navigate(action)
@@ -45,16 +50,22 @@ class NoteAdapter(context: Context):
         }
     }
 
-    private fun setAnimation(viewToAnimate: View, position: Int) {
+    private fun setAnimation(viewToAnimate: View, position: Int, options: Boolean) {
         // If the bound view wasn't previously displayed on screen, it's animated
-        if (position > lastPosition) {
+        if (options) {
+            if (position > lastPosition) {
+                val animation = AnimationUtils.loadAnimation(context, R.anim.anim_bottom_to_top)
+                viewToAnimate.startAnimation(animation)
+                lastPosition = position
+            }
+        } else {
             val animation = AnimationUtils.loadAnimation(context, R.anim.anim_bottom_to_top)
             viewToAnimate.startAnimation(animation)
-            lastPosition = position
         }
     }
-    private fun ConvertIntToColor(i: Int):Int{
-        when(i){
+
+    private fun ConvertIntToColor(i: Int): Int {
+        when (i) {
             0 -> return context.getColor(R.color.colorGreen)
             1 -> return context.getColor(R.color.colorYellow)
             2 -> return context.getColor(R.color.colorRed)
@@ -62,9 +73,45 @@ class NoteAdapter(context: Context):
         return -1;
     }
 
-    fun setData(list:List<Note>){
-        this.list = list
-        notifyDataSetChanged()
+    private var filter = object : Filter() {
+        //run Worker Thread
+
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            val list = ArrayList<Note>()
+            if (constraint!!.isEmpty()) {
+                list.addAll(fullList)
+                Log.d("TAG", "list: " + list.size + " text is empty ")
+            } else {
+                for (item in fullList) {
+                    if (item.title.toLowerCase(Locale.ROOT)
+                            .contains(constraint.toString().toLowerCase(Locale.ROOT))
+                    )
+                        list.add(item)
+                }
+                Log.d("TAG", "list: " + list.size + " text: " + constraint)
+            }
+            val reusult = FilterResults()
+            reusult.values = list
+            return reusult
+        }
+
+        //run UI thread
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+            (list as ArrayList).clear()
+            try {
+                (list as ArrayList<Note>).addAll(results?.values as Collection<Note>)
+                Log.d("TAG", "publishResults: " + list.size)
+            } catch (e: Exception) {
+
+            }
+
+            notifyDataSetChanged()
+        }
+
+    }
+
+    override fun getFilter(): Filter {
+        return filter
     }
 
 }
--- app/src/main/java/com/example/mynote/Fragment/main_screen.kt
+++ app/src/main/java/com/example/mynote/Fragment/main_screen.kt
@@ -3,11 +3,14 @@ package com.example.mynote.Fragment
 import android.app.AlertDialog
 import android.app.Application
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.widget.AdapterView
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -20,6 +23,7 @@ import com.example.mynote.R
 import com.example.mynote.Room.Note
 import com.example.mynote.Room.NoteViewModel
 import kotlinx.android.synthetic.main.fragment_edit_note.view.*
+import kotlinx.android.synthetic.main.fragment_main_screen.*
 import kotlinx.android.synthetic.main.fragment_main_screen.view.*
 
 
@@ -46,9 +50,9 @@ class main_screen : Fragment() {
         initComponents()
         view.fltAdd.setOnClickListener {
             val action = main_screenDirections.actionMainScreenToEditNote(null)
-            Navigation.findNavController(requireActivity(),R.id.fragment).navigate(action);
+            Navigation.findNavController(requireActivity(), R.id.fragment).navigate(action);
         }
-        view?.fltDeleteAll?.setOnClickListener {
+        view.fltDeleteAll?.setOnClickListener {
             val dialog = AlertDialog.Builder(context)
             dialog.setTitle("NOTIFY")
             dialog.setMessage("Do you want delete it ?")
@@ -63,18 +67,69 @@ class main_screen : Fragment() {
             dialog.show()
 
         }
+        spinnerPriority.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when (position) {
+                    0 -> {
+                        noteViewModel.getAllListNote.observe(viewLifecycleOwner, {
+                            list?.clear()
+                            list?.addAll(it)
+                            adapter.notifyDataSetChanged()
+                        })
+                    }
+                    1 -> {
+                        noteViewModel.getALlListLowToHigh.observe(viewLifecycleOwner, {
+                            list?.clear()
+                            list?.addAll(it)
+                            adapter.notifyDataSetChanged()
+                        })
+                    }
+                    else -> {
+                        noteViewModel.getALlListHighToLow.observe(viewLifecycleOwner, {
+                            list?.clear()
+                            list?.addAll(it)
+                            adapter.notifyDataSetChanged()
+                        })
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+        view.edtSearch.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                adapter.filter.filter(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
         super.onViewCreated(view, savedInstanceState)
     }
 
     private fun initComponents(){
         list = ArrayList()
-        adapter = context?.let { NoteAdapter(it) }!!
-        noteViewModel.getAllListNote.observe(viewLifecycleOwner, {
-           adapter.setData(it)
-        })
         val layout = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
         view?.listNote?.layoutManager = layout
-        view?.listNote?.adapter = adapter
+        noteViewModel.getAllListNote.observe(viewLifecycleOwner, {
+            list!!.addAll(it)
+            adapter = context?.let { it1 -> NoteAdapter(it1, list!!) }!!
+            view?.listNote?.adapter = adapter
+        })
     }
 
 
